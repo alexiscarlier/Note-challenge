57d22d6d455f64a021c51967b613543f948d8e87
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-10 15:27:30 +0000

    Added lots of functionality, notes can now be added and viewed in long form

--- note-controller.js
+++ note-controller.js
@@ -7,13 +7,12 @@
 // }
 //
 // helloToHowdy();
-
-
 (function(exports) {
 
-  function NoteController(noteList) {
-    noteList.createNote("Favourite drink: seltzer");
-    this._view = new View(noteList);
+  function NoteController() {
+    this._noteList = new NoteList();
+    // this._noteList.createNote("Favourite drink: seltzer");
+    this._view = new View(this._noteList);
   }
 
   NoteController.prototype.getView = function() {
@@ -26,5 +25,52 @@
     element.innerHTML = currentView.display();
   };
 
+
+  // function makeUrlShowFullNote() {
+  //   window.addEventListener("hashchange", showFullNoteForPage);
+  // }
+
+  NoteController.prototype.makeUrlShowFullNote = function() {
+    window.addEventListener("hashchange", showFullNoteForPage);
+  };
+
+  function showFullNoteForPage() {
+    showNote(getNoteFromUrl(window.location));
+  }
+
+  function getNoteFromUrl(location) {
+    console.log(location);
+    return location.hash.split("/")[1];
+  }
+
+  function showNote(note) {
+    document
+      .getElementById("full-note")
+      .innerHTML = noteController._noteList.getNotes()[parseInt(note)].showContents();
+  }
+
+
+
+  NoteController.prototype.submitListener = function() {
+    form = document.getElementById("text");
+    form.addEventListener("click", function(event) {
+      event.preventDefault();
+      noteContents = document.getElementById("note-text").value;
+      noteController._noteList.createNote(noteContents);
+      noteController.getAndUseHTML();
+    });
+  };
+
+
   exports.NoteController = NoteController;
+})(this);
+
+
+(function(exports) {
+  window.onload = function() {
+    noteController = new NoteController();
+    noteController.submitListener();
+    noteController.makeUrlShowFullNote();
+    noteController.getAndUseHTML();
+  };
 })(this);
--- tests/note-controller-test.js
+++ tests/note-controller-test.js
@@ -10,10 +10,16 @@ var assert = {
 
 
 function correctInnerHTML() {
-  noteList = new NoteList();
-  noteController = new NoteController(noteList);
+  noteController = new NoteController();
   noteController.getAndUseHTML();
   assert.isTrue(document.getElementById("app").innerHTML === '<ul><li><div><a href="#notes/0">Favourite drink: sel</a></div></li></ul>');
 }
 
 correctInnerHTML();
+
+function displaysNote() {
+  noteList = new NoteList();
+  noteController = new NoteController(noteList);
+}
+
+displaysNote();
